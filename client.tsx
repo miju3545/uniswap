17f643a8f6eb1bdd100de178e7679e9122d48bec
rovxxmjxx <miju3545@gmail.com>
2023-02-24 01:13:54 +0900

    fix: app render method 변경

--- client.tsx
+++ client.tsx
@@ -1,14 +1,15 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './src/App';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './src/App';
-
-render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('app')
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
