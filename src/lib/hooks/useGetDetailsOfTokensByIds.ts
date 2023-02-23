8bb9c3ca98d20c157ef54ec39e48a93a69143b96
rovxxmjxx <miju3545@gmail.com>
2023-02-24 04:01:34 +0900

    feat: get token info(usd) api/fetcher

---
+++ src/lib/hooks/useGetDetailsOfTokensByIds.ts
@@ -0,0 +1,16 @@
+import { fetcher, QueryKeys } from '@lib/queryClient';
+import { useQueries } from 'react-query';
+import debounce from '../debounce';
+
+const fetchTokenById = async (id: string) => {
+  return await fetcher({
+    method: 'GET',
+    params: { vs_currencies: 'USD', ids: id },
+  });
+};
+
+const useGetDetailsOfTokensByIds = (...ids: string[]) => {
+  return useQueries(ids.map((id: string) => ({ queryKey: [QueryKeys, id], queryFn: () => fetchTokenById(id) })));
+};
+
+export default useGetDetailsOfTokensByIds;
