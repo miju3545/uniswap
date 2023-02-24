49faa03c8f5ec7befbc103be4513f99308f67794
rovxxmjxx <miju3545@gmail.com>
2023-02-25 00:14:54 +0900

    feat: constants

---
+++ src/config/token-list.ts
@@ -0,0 +1,34 @@
+const tokenList: { [key: string]: { id: string } } = {
+  ETH: {
+    id: 'ethereum',
+  },
+  USDT: {
+    id: 'tether',
+  },
+  USDC: {
+    id: 'usd-coin',
+  },
+  DAI: {
+    id: 'dai',
+  },
+  AAVE: {
+    id: 'aave',
+  },
+  WBTC: {
+    id: 'bitcoin',
+  },
+  AXS: {
+    id: 'axie-infinity',
+  },
+  COMP: {
+    id: 'compound-coin',
+  },
+  CRV: {
+    id: 'curve-dao-token',
+  },
+  ENS: {
+    id: 'ethereum-name-service',
+  },
+};
+
+export default tokenList;
