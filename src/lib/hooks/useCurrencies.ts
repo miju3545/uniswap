bddcb0936cd6e1d05b3a64ebbbebbdbbb2e0e450
rovxxmjxx <miju3545@gmail.com>
2023-02-25 00:17:23 +0900

    chore: 파일 삭제

--- src/lib/hooks/useCurrencies.ts
+++ src/lib/hooks/useCurrencies.ts
@@ -4,36 +4,42 @@ export const formatCurrency = ({
   amount,
   currencyCode,
   locale,
+  fraction,
 }: {
   amount: number;
   currencyCode: string;
   locale: string;
+  fraction: number;
 }): string => {
   const formatCurrency = new Intl.NumberFormat(locale, {
     style: 'currency',
     currency: currencyCode,
-    maximumFractionDigits: 2,
+    maximumFractionDigits: fraction,
     currencyDisplay: 'narrowSymbol',
   });
 
   return formatCurrency.format(amount);
 };
 
-export const calculateCurrency = (data: {
+export const currencyFormatter = (data: {
   amount: number;
   currencyCode?: string;
   locale?: string;
-}): { result: string } => {
-  const { amount, currencyCode = 'USD', locale = 'en-US' } = data ?? {};
-  const value = useMemo(() => formatCurrency({ amount, currencyCode, locale }), [amount, currencyCode, locale]);
+  fraction?: number;
+}): string => {
+  const { amount, currencyCode = 'USD', locale = 'en-US', fraction = 2 } = data ?? {};
+  const value = useMemo(
+    () => formatCurrency({ amount, currencyCode, locale, fraction }),
+    [amount, currencyCode, locale, fraction]
+  );
 
-  return { result: value };
+  return value;
 };
 
 const useCurrencies = (...datas: { amount: number; price: number; currencyCode?: string; locale?: string }[]) => {
   const values = datas.map((data) => {
     const { amount, price, ...rest } = data;
-    return calculateCurrency({ amount: amount * price, ...rest }).result;
+    return currencyFormatter({ amount: amount * price, ...rest });
   });
 
   return values;
--- src/lib/hooks/useSwitchableInputs.ts
+++ src/lib/hooks/useSwitchableInputs.ts
@@ -1,30 +1,30 @@
-import { useState, useEffect, useMemo, useCallback } from 'react';
-import useInput, { ReturnTypes } from './useInput';
+// import { useState, useEffect, useMemo, useCallback } from 'react';
+// import useInput, { ReturnTypes } from './useInput';
 
-const useSwitchableInput = (): {
-  from: ReturnTypes<any>;
-  into: ReturnTypes<any>;
-  onSwitch: () => void;
-  onReset: () => void;
-} => {
-  let _from = useInput(0);
-  let _into = useInput(0);
-  const [[from, into], setValue] = useState([_from, _into]);
+// const useSwitchableInput = (): {
+//   from: ReturnTypes<any>;
+//   into: ReturnTypes<any>;
+//   onSwitch: () => void;
+//   onReset: () => void;
+// } => {
+//   let _from = useInput(0);
+//   let _into = useInput(0);
+//   const [[from, into], setValue] = useState([_from, _into]);
 
-  const onSwitch = () => {
-    setValue(([a, b]) => [b, a]);
-  };
+//   const onSwitch = () => {
+//     setValue(([a, b]) => [b, a]);
+//   };
 
-  const onReset = () => {
-    from.onReset();
-    into.onReset();
-  };
+//   const onReset = () => {
+//     from.onReset();
+//     into.onReset();
+//   };
 
-  useEffect(() => {
-    setValue([_from, _into]);
-  }, [_from.value, _into.value]);
+//   useEffect(() => {
+//     setValue([_from, _into]);
+//   }, [_from.value, _into.value]);
 
-  return { from, into, onSwitch, onReset };
-};
+//   return { from, into, onSwitch, onReset };
+// };
 
-export default useSwitchableInput;
+// export default useSwitchableInput;
