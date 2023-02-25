4bebea89e5bfc48309da3adda7d6c9eb3f310ad9
rovxxmjxx <miju3545@gmail.com>
2023-02-25 12:24:13 +0900

    refactor: rename page name

--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import OverView from '@components/token/TokenOverView';
-
-const Index: FC = () => {
-  return <OverView />;
-};
-
-export default Index;
---
+++ src/pages/swap.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import SwapTokenView from '@components/swap/SwapTokenView';
+
+const SwapTokenPage: FC = () => {
+  return <SwapTokenView />;
+};
+
+export default SwapTokenPage;
